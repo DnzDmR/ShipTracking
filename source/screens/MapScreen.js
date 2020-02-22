e8c2c354ad25e7701d472a062709e55f5e3128eb
Denizcan <denizcandemir@outlook.com>
2020-02-22 17:00:47 +0300

    google maps added

--- source/screens/MapScreen.js
+++ source/screens/MapScreen.js
@@ -1,9 +1,36 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {View,StyleSheet} from 'react-native';
+import MapView from 'react-native-maps';
+
+
+const styles = StyleSheet.create({
+    container: {
+      ...StyleSheet.absoluteFillObject,
+      height: 600,
+      width: 400,
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+    },
+    map: {
+      ...StyleSheet.absoluteFillObject,
+    },
+   });
+
 
 const MapScreen = () => {
     return(
-        <Text>Map</Text>
+        <View style={styles.container}>
+            <MapView
+            style={styles.map}
+            region={{
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.015,
+                longitudeDelta: 0.0121,
+            }}
+            >
+            </MapView>
+      </View>
     );
 
 }
