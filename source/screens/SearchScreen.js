b18c880110454efecd23004f2463257368b9f61e
Denizcan <denizcandemir@outlook.com>
2020-02-23 17:17:22 +0300

    code refactoring

--- source/screens/SearchScreen.js
+++ source/screens/SearchScreen.js
@@ -25,7 +25,7 @@ export default class SearchScreen extends Component{
         return(
             <View>
                 
-                <SearchBar onSubmitEditing={this.getInfo.bind(this)} placeholder="IMO" maxLength={6} keyboardType="numeric" onChangeText={this.updateSearch} value={search} />
+                <SearchBar onSubmitEditing={this.getInfo.bind(this)} placeholder="IMO" maxLength={7} keyboardType="numeric" onChangeText={this.updateSearch} value={search} />
                         
                 { this.state.ship == null ? <Text>Test IMO : 278335 </Text> : <ShipDetail base={this} ship={this.state.ship}></ShipDetail>}
 
